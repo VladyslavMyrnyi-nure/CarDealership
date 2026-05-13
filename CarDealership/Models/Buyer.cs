2db6158ca3d23cf17eeefb9683ab58cb54c72bae
1tsfranxx <114751845+1tsfranxxD@users.noreply.github.com>
2026-05-13 22:08:33 +0300

    Додано моделі машини та покупця. Додано характеристики машини

---
+++ CarDealership/Models/Buyer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CarDealership.Enums;
+
+namespace CarDealership.Models
+{
+    /// <summary>
+    /// Клас покупця
+    /// </summary>
+    public class Buyer
+    {
+        public Guid Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string DesiredBrand { get; set; }
+
+        public int MinYear { get; set; }
+
+        public decimal MaxPrice { get; set; }
+
+        public TechnicalCondition RequiredCondition { get; set; }
+
+        public Buyer()
+        {
+            Id = Guid.NewGuid();
+
+            FullName = string.Empty;
+            Phone = string.Empty;
+            Email = string.Empty;
+            DesiredBrand = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{FullName} - {DesiredBrand}";
+        }
+    }
+}
---
+++ CarDealership/Models/Car.cs
@@ -0,0 +1,41 @@
+﻿using CarDealership.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public bool IsNew { get; set; }
+        public double EngineVolume { get; set; }
+        public int HorsePower { get; set; }
+        public FuelType FuelType { get; set; }
+        public TransmissionType Transmission { get; set; }
+        public int Muileage { get; set; }
+        public TechnicalCondition Condition { get; set; }
+        public string Features { get; set; }
+        public decimal Price { get; set; }
+        public Car()
+        {
+                Id = Guid.NewGuid();
+
+            Brand = string.Empty;
+            Model = string.Empty;
+            Country = string.Empty;
+            Features = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{Brand} {Model} ({Year})";                
+        }
+    }
+}
